440b92c9093a3810d01c7def99acf4d4bb2cc926
k-19 <sergeifilinovich01@gmail.com>
2022-10-13 19:55:42 +0300

    init

---
+++ src/main/java/by/k19/edu/EduApplication.java
@@ -0,0 +1,19 @@
+package by.k19.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class EduApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EduApplication.class);
+    }
+}
